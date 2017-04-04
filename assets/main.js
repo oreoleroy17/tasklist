7455ef03c551c0f7ff8422dd1cc55ac2959c3bf0
oreoleroy17 <kyftour@gmail.com>
2017-04-04 16:32:53 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,6 @@
 //Anonymous function
 
 $(function() {
-//import objTraverse from 'obj-traverse'
 
 /*
 This listchecker requires two custom fields.
@@ -37,9 +36,6 @@ client.get('ticket.assignee.group.name').then(function(o){
   })
 })
 
-  //client.invoke('resize', { width: '100%', height: '250' });
-  //client.invoke('ticketFields:custom_field_31548757.show');
-
   client.on('app.registered', function(){
     console.log('app registered');
     // GET CUSTOM FIELD ID FROM SETTINGS
@@ -55,11 +51,10 @@ client.get('ticket.assignee.group.name').then(function(o){
 
   });
 
+//DOM Events
   $(document).on('click','.css-checkbox',prepareTasksForStore);
   $(document).on('click','.cmn-toggle',showCustomField);
-
-
-
+  
 // *******  ZAF V2 = prevent Ticket save  *******
 /*
 client.on('ticket.save', function() {
@@ -78,7 +73,7 @@ client.metadata().then(function(metadata) {
     var taskGroupNamesFieldID = metadata.settings['taskGroupNamesFieldID'];
   // Get data from custom field
   client.get('ticket.customField:custom_field_'+storeTasksFieldID).then(function(arr) {
-    //client.get('ticketFields:custom_field_51849927.options').then(function(items){
+    
       client.get('ticket.customField:custom_field_'+taskGroupNamesFieldID).then(function(o){
 
             if(arr['ticket.customField:custom_field_'+storeTasksFieldID]){
@@ -92,14 +87,14 @@ client.metadata().then(function(metadata) {
 
                               }else{
                                 //get Percent complete
-                                //console.log(singleTaskList.Tasks[2].updated);
+                               
                                 var objTasksCompleted = getTaskProgress(singleTaskList.Tasks, 'progress');
                                 var objTasksModified = getModifiedTasks(singleTaskList.Tasks);
                                 //console.log(objTasksCompleted);
                                 var viewData = {listname: o['ticket.customField:custom_field_'+taskGroupNamesFieldID], tasks: objTasksModified, percentSolved: objTasksCompleted[0], alltaskscomplete: objTasksCompleted[1]};
                                 var templateUrl = "tasks.hdbs";
                                 //console.log(viewData);
-                                //switchNavTo('assign');
+                          
                                 switchView(templateUrl, viewData);
 
                               }
@@ -121,7 +116,7 @@ client.metadata().then(function(metadata) {
                       var viewData = {listname: o['ticket.customField:custom_field_'+taskGroupNamesFieldID], tasks: singleTaskList.Tasks, percentSolved: objTasksCompleted[0], alltaskscomplete: objTasksCompleted[1] };
                       var templateUrl = "tasks.hdbs";
                       //console.log(viewData);
-                      //switchNavTo('assign');
+                 
                       switchView(templateUrl, viewData);
 
                     }
@@ -155,37 +150,25 @@ function showCustomField(){
         }
       })
 }
-  /*
-  findAndModifyFirst(tree, childrenKey, objToFindBy, replacementObj)
 
-  tree = JSON object with parent and children
-  childrenKey = 'Tasks' or the key name of the children objects
-  objToFindBy = { name: value }
-  replacementObj = {newname: new value}
-
-
-  */
 //****************************************************************************
-// MODIFY AND REPLACE FUNCTION FOR SUBTASKS AND FULL TASK LIST = DO NOT CHANGE
+// MODIFY AND REPLACE FUNCTION FOR SUBTASKS AND FULL TASK LIST 
 //****************************************************************************
   const findInChildren = (obj, childrenKey, objToFindBy, replacementObj) => {
   console.log('findInChildren CALLED');
-    //console.log(objToFindBy);
-    //console.log(replacementObj);
+    
     let findSuccess = false;
     let modifiedObj = false;
     const findChildrenKeys = Object.keys(objToFindBy);
+    
     if (obj.hasOwnProperty(childrenKey)) {
-      //console.log('has childrenKey');
-      //console.log(obj[childrenKey].length);
+     
       for (let i = 0; i < obj[childrenKey].length; i++) {
         findChildrenKeys.forEach((key) => {
 
           // check for null values
           if(obj[childrenKey][i][key] != '' && objToFindBy[key] != ''){
-            //console.log(key);
-          //  console.log(obj[childrenKey][i][key]);
-            //console.log(objToFindBy[key]);
+          
               if(key == 'name'){
                   _.isEqual(obj[childrenKey][i][key], objToFindBy[key]) ? findSuccess = true : findSuccess = false;
               }
@@ -195,23 +178,21 @@ function showCustomField(){
             });
 
         if (findSuccess) {
-          //console.log('replace child');
-          //console.log(obj[childrenKey][i]);
-          //console.log(replacementObj);
+    
 
           obj[childrenKey][i] = replacementObj;
-          //console.log(obj[childrenKey][i]);
+         
           modifiedObj = true;
           break;
         }
       }
-      //console.log(modifiedObj);
+    
       if (!findSuccess) {
-        //console.log('no success');
+      
         obj[childrenKey].forEach(child => findInChildren(child, childrenKey, objToFindBy, replacementObj));
       }
     } else if (modifiedObj != true){
-      //console.log(obj.length);
+      
       for (let i = 0; i < obj.length; i++) {
 
          findInChildren(obj[i], childrenKey, objToFindBy, replacementObj);
@@ -235,27 +216,24 @@ function prepareTasksForStore(){
     var singleTaskList;
     var d = new Date();
     var strDateTime = _.trim(formatDate(d));
-    //console.log(strDateTime);
+   
 
 
     // get checkbox form elements
     var inputs = $(' :input:not([type=button]) ');
-    //console.log(inputs);
-    //var inputs = this.$(" :input:not([type=button], .checkAll, .removeAll) ");
-    //hidden field listname
-    //console.log(inputs[0].name);
-
+   
     //childrenKey used as search value in ModifyandFindFirst function
     var childrenKey = 'Tasks';
 
     // GET CUSTOM FIELD ID FROM SETTINGS
     client.metadata().then(function(metadata) {
-        //console.log(metadata.settings['storeID']);
+     
         var storeTasksFieldID = metadata.settings['storeTasksFieldID'];
         var taskGroupNamesFieldID = metadata.settings['taskGroupNamesFieldID'];
+      
     // Get data from custom field
     client.get('ticket.customField:custom_field_'+storeTasksFieldID).then(function(data) {
-    //client.get('ticketFields:custom_field_51849927.options').then(function(items){
+    
       client.get('ticket.customField:custom_field_'+taskGroupNamesFieldID).then(function(o){
 
 
@@ -264,24 +242,17 @@ function prepareTasksForStore(){
 
             //get total array from custom field task list
                  arr = JSON.parse(data['ticket.customField:custom_field_'+storeTasksFieldID]);
-                //console.log(arr);
+               
 
                 // pull single task list
                 singleTaskList = getSingleTaskList(arr, o['ticket.customField:custom_field_'+taskGroupNamesFieldID]);
-                //console.log(singleTaskList);
-// update value using findInChildren, return updated object
-// replace old sub list in full object using modifyandfindfirst function
-
-
+         
                 _.each(inputs, function(item){
-                  //console.log(item.name);
-                  //console.log(this.$(item).prop('checked'));
-                  //console.log(item.value);
-
+            
 
                   //if checkbox is checked and checkbox value (o.value) is '', then new checked
                       if(this.$(item).prop('checked')==true && item.value==''){
-                        //console.log('new check!');
+                        
 
                           objExisting = {id: item.id, name: item.name, value: ''};
                           objReplace = {id: item.id, name: item.name, value: true, updated: strDateTime};
@@ -290,16 +261,8 @@ function prepareTasksForStore(){
                         //update value in sub task list
                           modSubTaskList = findInChildren(singleTaskList, childrenKey, objExisting, objReplace);
 
-                          //console.log(objOldList);
-                          //console.log(modSubTaskList);
-
-                        //  modFullList = findAndModifyFirst(arr, o['ticket.customField:custom_field_51849927'], objOldList, modSubTaskList );
-
-                        //console.log(modFullList);
-
-
                       }else if(this.$(item).prop('checked')==false && item.value=='checked'){
-                        //console.log('uncheck!');
+                  
                       //if checkbox is not checked but checkbox value is checked, checkbox was unchecked
 
                           objExisting = {id: item.id, name: item.name, value: true};
@@ -308,9 +271,7 @@ function prepareTasksForStore(){
                           //update value in sub task list
                             modSubTaskList = findInChildren(singleTaskList, childrenKey, objExisting, objReplace);
 
-                            //console.log(objOldList);
-                          //  console.log(modSubTaskList);
-
+                          
                       }else{
 
                       console.log('no changes were made');
@@ -326,45 +287,34 @@ function prepareTasksForStore(){
                 client.get('ticket.customField:custom_field_'+taskGroupNamesFieldID).then(function(o){
 
                  listarr = list;
-                //console.log(listarr);
+               
                 // pull single task list
                 singleTaskList = getSingleTaskList(listarr, o['ticket.customField:custom_field_'+taskGroupNamesFieldID]);
-                //console.log(singleTaskList);
-
+            
                 _.each(inputs, function(item){
-                  //console.log(item.name);
-                  //console.log(this.$(item).prop('checked'));
+             
                       if(this.$(item).prop('checked')==true ){
 
                           objExisting = {id: item.id, name: item.name, value: ''};
                           objReplace = {id: item.id, name: item.name, value: true, updated: strDateTime};
 
-                          //console.log(objExisting);
-                          //console.log(objReplace);
-
-                          //update value in sub task list
+                         //update value in sub task list
                           modSubTaskList = findInChildren(singleTaskList, childrenKey, objExisting, objReplace);
 
-                        //  console.log(singleTaskList);
-                          //console.log(modSubTaskList);
-
-                          //modFullList = findAndModifyFirst(arr, o['ticket.customField:custom_field_51849927'], singleTaskList, modSubTaskList );
-
-                          //console.log(modFullList);
 
                       }
                     })
                     //console.log('line 212');
                     client.get('ticket.id').then(function(id){
-                        //console.log(arr);
+                   
                         storeTasks(listarr, id['ticket.id'], storeTasksFieldID, taskGroupNamesFieldID);
                       })
             })
           })
           }
-//console.log('line 220');
+
           client.get('ticket.id').then(function(id){
-              //console.log(arr);
+             
               storeTasks(arr, id['ticket.id'], storeTasksFieldID, taskGroupNamesFieldID);
             })
     })
@@ -378,26 +328,13 @@ function storeTasks(data, ticketID, storeFieldID,taskGroupNamesFieldID){
          ****************************************/
         console.log('storeTasks called');
 
-            // load the ticket object from Zendesk
-          //  var ticket = this.ticket();
-          //  var ticketstatus = this.ticket().status();
-          //var ticketID = data[0].value;
-
-          //console.log(ticketID);
-                    //var ticketStatus = client.get('ticket.status');
-          //console.log(ticketID);
-          //console.log(ticketStatus);
-          //console.log(data);
           client.get('ticket.status').then(function(status) {
-                //console.log(arr['ticket.status']);
-                //console.log(ticketID);
+               
 
     var ticketStatus = status['ticket.status'];
     if(ticketStatus !== 'closed'){
 
-          //To Do => replace Custom Field ID with dynamic ID
-
-          //client.set('ticket.customField:custom_field_31548757', '');
+         
             var dat = {"ticket": {"custom_fields": [{"id": storeFieldID, "value": JSON.stringify(data)  }]}};
 
 
@@ -413,17 +350,11 @@ function storeTasks(data, ticketID, storeFieldID,taskGroupNamesFieldID){
                   client.request(settings).then(function(d) {
 
 
-                              //console.log(d.ticket.fields[6].value);
-
-                                        var msg = "Tasks were successfully updated!";
-                                        console.log(msg);
-                                        //client.invoke('ticketFields:custom_field_31548757.show');
+                                   var msg = "Tasks were successfully updated!";
+                                        
                                         client.invoke('notify', [msg]);
                                           showSingleTaskList(JSON.parse(d.ticket.fields[6].value));
-                                          //client.invoke('ticketFields:custom_field_31548757.show');
-                                          //this.ticketFields(this.customFieldLabel()).show();
-                              //showInfo(data);
-                              //showNew(data);
+                            
                             },
                             function(response) {
                               console.log(response);
@@ -437,26 +368,20 @@ function storeTasks(data, ticketID, storeFieldID,taskGroupNamesFieldID){
          }else{
 
             var errmsg = "Tasks cannot be added to closed tickets!";
-            //services.notify(errmsg, 'error');
-          //  this.assignNewTaskList();
+        
           }
-            /*
-            ticket.customField("custom_field_24680", "yes"); // type: checkbox
-            ticket.customField("custom_field_34567", "vip"); // type: tagger
-            */
+          
           }) //end ticket status promise
       }
 
 function showSingleTaskList(objFullList, storeFieldID, taskGroupFieldID){
           console.log('showSingleTaskList called');
-          //objKanbanStates = getKanbanStates(kanbanoptions, o['ticket.customField:custom_field_51849927']);
+          
 
           if (objFullList != null){
             client.get('ticket.customField:custom_field_'+taskGroupFieldID).then(function(o){
                       singleTaskList = getSingleTaskList(objFullList, o['ticket.customField:custom_field_'+taskGroupFieldID]);
-                      //console.log(singleTaskList);
-
-                      //console.log(JSON.stringify(singleTaskList));
+                                        
                       if(singleTaskList == null){
 
                         console.log('no lists match kanban state, empty list');
@@ -466,10 +391,10 @@ function showSingleTaskList(objFullList, storeFieldID, taskGroupFieldID){
                         var objTasksCompleted = getTaskProgress(singleTaskList.Tasks, 'progress');
                         var objTasksModified = getModifiedTasks(singleTaskList.Tasks);
                         var viewData = {listname: o['ticket.customField:custom_field_'+taskGroupFieldID], tasks: objTasksModified, percentSolved: objTasksCompleted[0], alltaskscomplete: objTasksCompleted[1] };
-                        //console.log(viewData);
+                       
                         var templateUrl = "tasks.hdbs";
-                        //console.log(viewData);
-                        //switchNavTo('assign');
+                      
+                  
                         switchView(templateUrl, viewData);
 
                       }
@@ -481,12 +406,8 @@ function showSingleTaskList(objFullList, storeFieldID, taskGroupFieldID){
           client.get('ticket.customField:custom_field_'+storeFieldID).then(function(arr) {
             client.get('ticket.customField:custom_field_'+taskGroupFieldID).then(function(o){
 
-
-                  //console.log(arr);
                 singleTaskList = getSingleTaskList(arr['ticket.customField:custom_field_'+storeFieldID], o['ticket.customField:custom_field_'+taskGroupFieldID]);
-                //console.log(singleTaskList);
-
-                //console.log(JSON.stringify(singleTaskList));
+         
                 if(singleTaskList == null){
 
                   console.log('no lists match kanban state, empty list');
@@ -499,7 +420,7 @@ function showSingleTaskList(objFullList, storeFieldID, taskGroupFieldID){
                   //console.log(viewData);
                   var templateUrl = "tasks.hdbs";
                   //console.log(viewData);
-                  //switchNavTo('assign');
+              
                   switchView(templateUrl, viewData);
 
                 }
@@ -511,14 +432,13 @@ function showSingleTaskList(objFullList, storeFieldID, taskGroupFieldID){
 
 function getSingleTaskList(arr, val){
     console.log('getSingleTaskList called');
-    //console.log(arr);
-    //console.log(val);
+
     var result = _.find(arr, function(item) {
 
                     return item.Name == val || item.listID == val || item.name == val;
 
                     });
-                    //console.log(result);
+                
        return result;
   }
   function getModifiedTasks(tasks){
@@ -531,12 +451,12 @@ function getSingleTaskList(arr, val){
        results = _.map(tasks, function(item){
 
         if(item.value == true){
-          //console.log('is checked');
+      
           checked = "checked";
           value = "checked";
 
         }else{
-          //console.log('not checked');
+          
             checked = "";
             value = "";
           }
@@ -611,7 +531,7 @@ function getSingleTaskList(arr, val){
 function getFullJsonObj() {
   console.log('getFullJsonObj called');
     var dataSource = 'lists.json';
-    // ORIGINAL -> jqueryNoConflict.getJSON(dataSource, renderDataVisualsTemplate);
+   
     return jqueryNoConflict.getJSON(dataSource); // can do callback if desired ...getJSON(data, callback fn)
 }
 
